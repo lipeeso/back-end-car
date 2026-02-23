61caff3cb7edc775423248f1042ed23bfdf3dd0a
lipeeso <lipeeso021@gmail.com>
2026-02-23 20:59:26 -0300

    Add pyproject.toml for project configuration and initialize tests package

---
+++ README.md
@@ -0,0 +1,1 @@
+1 - Para  rodar o projeto local executar o comando: poetry run fastapi dev car_api/app.py ou pode ser pelo shell do poetry, basta digitar o comando: poetry shell e depois fastapi dev car_api/app.py
---
+++ car_api/routers/users.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, status
+
+
+router = APIRouter()
+
+@router.get('/', status_code=status.HTTP_200_OK)
+async def list_users():
+     return {
+        'users': [
+            {
+                'id': 1,
+                'email': 'pycodebr@gmail.com',
+            },
+            {
+                'id': 2,
+                'email': 'joao@gmail.com',
+            },
+            {
+                'id': 3,
+                'email': 'mario@gmail.com',
+            },
+        ]
+    }
