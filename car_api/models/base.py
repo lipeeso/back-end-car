5056d76d15fdbec2835fdbd67d9ccadf8f508c1e
lipeeso <lipeeso021@gmail.com>
2026-03-03 22:24:43 -0300

    implementation models

---
+++ car_api/models/base.py
@@ -0,0 +1,4 @@
+from sqlalchemy.orm import declarative_base
+'''Essa classe é a base para todas as outras classes do modelo. Ela é usada para criar as tabelas no banco de dados.'''
+class Base(declarative_base):
+    pass
