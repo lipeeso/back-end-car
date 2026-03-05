f301a4452d4f69617de8e6c3c27b77d52cd4e7b6
lipeeso <lipeeso021@gmail.com>
2026-03-05 17:19:54 -0300

    implementation models

---
+++ car_api/models/__init__.py
@@ -0,0 +1,8 @@
+'''dunder init
+Para facilitar a migração'''
+
+from car_api.models.base import Base
+
+from car_api.models.users import User
+
+__all__ = ['Base', 'User']
---
+++ car_api/models/users.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import func #-> função de banco de dados
+from sqlalchemy.orm import mapped_column, Mapped
+from car_api.models import Base
+
+
+class User(Base):
+    __tablename__ = 'users' # -> Nome da tabela que vai ser criado
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] #-> não tem uma propriedade especial no campo então pode deixar assim
+    email: Mapped[str] = mapped_column(unique=True)
+    update_at = Mapped[datetime] = mapped_column(
+        onupdate=func.now(), server_default=func.now()
+    )
+    created_at = Mapped[datetime] = mapped_column(
+        server_default=func.now()
+    )
