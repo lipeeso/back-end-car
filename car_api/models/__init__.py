b3a31dfa3f98bd8930296eed3fc22047b41e42ad
lipeeso <lipeeso021@gmail.com>
2026-03-26 10:35:37 -0300

    correction

--- car_api/models/__init__.py
+++ car_api/models/__init__.py
@@ -5,8 +5,6 @@ from car_api.models.base import Base
 
 from car_api.models.users import User
 
-from car_api.models.cars import Brand
-
-from car_api.models.cars import Car
+from car_api.models.cars import (Brand, Car)
 
 __all__ = ['Base', 'User', 'Brand', 'Car']
