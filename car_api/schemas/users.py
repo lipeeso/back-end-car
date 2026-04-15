aeda76db1972826fb1ef2a674c97c3b305616365
lipeeso <lipeeso021@gmail.com>
2026-04-14 21:50:59 -0300

    Update user schemas to include validation for username and password length

--- car_api/schemas/users.py
+++ car_api/schemas/users.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
 from datetime import datetime
 """Para diferentes use cases podemos criar diferentes schemas, por exemplo, para criar um usuário, atualizar um usuário, etc."""
 
@@ -9,7 +9,20 @@ class UserSchema(BaseModel):
     email: EmailStr
     password: str
 
+    @field_validator("username")
+    def username_min_lenght(cls, value):
+        if len(value) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        
+        return value
 
+    @field_validator("password")
+    def password_min_lenght(cls, value):
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        
+        return value
+    
 class UserUpdateSchema(
     BaseModel
 ): 
@@ -17,16 +30,30 @@ class UserUpdateSchema(
     email: Optional[EmailStr] = None
     password: Optional[str] = None
 
+    @field_validator("username")
+    def username_min_lenght(cls, value):
+        if len(value) < 3:
+            raise ValueError("Username must be at least 3 characters long")
+        
+        return value
+
+    @field_validator("password")
+    def password_min_lenght(cls, value):
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        
+        return value
+
 
 class UserPublicSchema(
     BaseModel
 ):
-    model_config = ConfigDict(from_attributes=True)  
+    model_config = ConfigDict(from_attributes=True)  #esse schema pode ser criado lendo atributos de um objeto, não só chaves de um dicionário. permite converter objetos ORM/classes em schema de resposta.
     id: int
     username: str
     email: EmailStr
     created_at: datetime
-    updated_at: datetime
+    update_at: datetime
 
 
 class UserListPublicSchema(
