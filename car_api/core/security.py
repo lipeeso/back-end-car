8dbd585eff034b94ff8010ec2c87d135c386cc8b
lipeeso <lipeeso021@gmail.com>
2026-05-15 19:44:46 -0300

    Add JWT token creation functionality with expiration handling

--- car_api/core/security.py
+++ car_api/core/security.py
@@ -1,4 +1,12 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict, Optional
+
 from pwdlib import PasswordHash
+import jwt
+
+from car_api.core.settings import Settings
+
+settings = Settings()
 
 pwd_context = PasswordHash.recommended()
 
@@ -8,3 +16,18 @@ def get_password_hash(password: str) -> str:
 '''Authentication for users'''
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data:Dict)-> str:
+
+    to_encode =  data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_EXPIRATION_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode, 
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
+    
+    return encoded_jwt
+
