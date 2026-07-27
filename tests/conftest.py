dee943bc4b6552e24ddb5def94cd0362121ee596
lipeeso <lipeeso021gmail.com>
2026-07-27 17:05:07 -0300

    Refactor conftest.py to include async session fixture and TestClient setup for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import pytest
 import pytest_asyncio
+from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+from car_api.app import app
+from car_api.core.database import get_session
 from car_api.models import Base
 
 
@@ -18,3 +22,16 @@ async def session():
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture
+def client(session):
+
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
