7d3103af0fa014eb75561ab793474a465aa710ec
lipeeso <lipeeso021gmail.com>
2026-07-29 10:55:03 -0300

    Add test for user creation in test_db.py

---
+++ tests/test_db.py
@@ -0,0 +1,33 @@
+import pytest
+from sqlalchemy import select
+
+from car_api.models import User
+
+
+@pytest.mark.asyncio
+async def test_create_user(session):
+
+    new_user = User(
+        username='testuser', password='testpassword', email='test@example.com'
+    )
+
+    session.add(new_user)
+    await session.commit()
+
+    user = await session.scalar(
+        select(User).where(User.email == 'test@example.com')
+    )
+
+    user_data = {
+        'id': user.id,
+        'username': user.username,
+        'password': user.password,
+        'email': user.email,
+    }
+
+    assert user_data == {
+        'id': 1,
+        'username': 'testuser',
+        'password': 'testpassword',
+        'email': 'test@example.com',
+    }
